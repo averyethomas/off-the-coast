c9909e17060924284acc8557e5bd857f746012d7
Avery Thomas <averyethomas@gmail.com>
2017-04-24 20:50:48 -0400

    add JS

--- deploy/scripts/app.js
+++ deploy/scripts/app.js
@@ -1,5 +1,58 @@
-var app = angular.module('angularApp', []);
+var app = angular.module('sharkApp', []);
 
-app.controller('mainCtrl', ['$scope', function($scope){
-    $scope.hello = 'Hi!';
+app.controller('mainCtrl', ['$scope', '$window', function($scope, $window){
+  
+  $scope.panels = document.getElementsByClassName('panel');
+  $scope.navItems = [];
+  
+  angular.forEach($scope.panels, function(value){
+    $scope.item = {link: value.id, position: value.offsetTop}
+    $scope.navItems.push($scope.item)
+   
+  });
+  
+  console.log($scope.navItems);
+
+  $scope.active = $scope.navItems[0];
+  
+  $scope.findActiveNav = function(){
+    
+    
+ 
+  };
+  
+  angular.element($window).bind('scroll', function(){
+    
+    $scope.scrollPos = $window.pageYOffset;
+    
+    
+//     if($scope.scrollPos < 100){
+//         console.log('test');
+//     } else if($scope.scrollPos > 100){
+//       console.log('bigger')
+//     }
+    if($scope.scrollPos < $scope.navItems[6].position){
+      console.log('notActive');
+    }else{
+      console.log('active');
+    }
+    
+    //console.log($scope.scrollPos);
+    
+  });
+
+  
+  
+//   $scope.bgChangePanels = document.getElementsByClassName('bgChange');
+//   $scope.bgChange = [];
+  
+//   angular.forEach($scope.bgChangePanels, function(value){
+//     $scope.bgChange.push(value.id)
+//   });
+  
+//   console.log(document.getElementById($scope.bgChange[1]).offsetTop);
+  
+  
+
 }]);
+
